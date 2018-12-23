86037a461c4f784e501cc393ad2600d92ca7195c
PinkHonkKongGmr <39237301+PinkHonkKongGmr@users.noreply.github.com>
2018-12-23 20:16:20 +0300

    Add files via upload

--- js/humburger.js
+++ js/humburger.js
@@ -5,6 +5,8 @@ var line1 = document.querySelector('.ln1');
 var line2 = document.querySelector('.ln2');
 var line3 = document.querySelector('.ln3');
 var slidemenu = document.querySelector('.slidemenu');
+var mobmenuBox = document.querySelector('.mobmenuBox');
+var mobmenu_wrapper = document.querySelector('.mobmenu_wrapper');
 
 
 
@@ -15,5 +17,6 @@ var slidemenu = document.querySelector('.slidemenu');
     line1.classList.toggle('cross');
     line2.classList.toggle('hide');
     line3.classList.toggle('cross2');
-
+    mobmenuBox.classList.toggle('mobmenuBoxNone');
+    mobmenu_wrapper.classList.toggle('white');
   });
