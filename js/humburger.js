41fab098cb52021592f6031de00cbc51d13f6e50
PinkHonkKongGmr <39237301+PinkHonkKongGmr@users.noreply.github.com>
2018-12-23 19:51:43 +0300

    Add files via upload

---
+++ js/humburger.js
@@ -0,0 +1,19 @@
+var humburger = document.querySelector('.humburger');
+
+
+var line1 = document.querySelector('.ln1');
+var line2 = document.querySelector('.ln2');
+var line3 = document.querySelector('.ln3');
+var slidemenu = document.querySelector('.slidemenu');
+
+
+
+
+
+  humburger.addEventListener('click',function humburgerToggle() {
+
+    line1.classList.toggle('cross');
+    line2.classList.toggle('hide');
+    line3.classList.toggle('cross2');
+
+  });
