61882be8f48193451657afb44c5c750d7687719d
PinkHonkKongGmr <39237301+PinkHonkKongGmr@users.noreply.github.com>
2019-01-09 12:31:29 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -39,59 +39,18 @@ for (var count = 0; count < pusher.length; count++) {
   _loop(count);
 }
 
-send.onclick = function() {
+'use strict';
+
+send.onclick = function () {
 
   if (input.value != '') {
-    var _iteratorNormalCompletion = true;
-    var _didIteratorError = false;
-    var _iteratorError = undefined;
-
-    try {
-      for (var _iterator = done[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-        var dn = _step.value;
-
-        dn.style.display = 'block';
-        input.style.background = 'inherit';
-      }
-    } catch (err) {
-      _didIteratorError = true;
-      _iteratorError = err;
-    } finally {
-      try {
-        if (!_iteratorNormalCompletion && _iterator.return) {
-          _iterator.return();
-        }
-      } finally {
-        if (_didIteratorError) {
-          throw _iteratorError;
-        }
-      }
+    for (var dn = 0; dn < done.length; dn++) {
+      done[dn].style.display = 'block';
+      input.style.background = 'inherit';
     }
-
-    setTimeout(function() {
-      var _iteratorNormalCompletion2 = true;
-      var _didIteratorError2 = false;
-      var _iteratorError2 = undefined;
-
-      try {
-        for (var _iterator2 = done[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
-          var dn = _step2.value;
-
-          dn.style.display = 'none';
-        }
-      } catch (err) {
-        _didIteratorError2 = true;
-        _iteratorError2 = err;
-      } finally {
-        try {
-          if (!_iteratorNormalCompletion2 && _iterator2.return) {
-            _iterator2.return();
-          }
-        } finally {
-          if (_didIteratorError2) {
-            throw _iteratorError2;
-          }
-        }
+    setTimeout(function () {
+      for (var _dn = 0; _dn < done.length; _dn++) {
+        done[_dn].style.display = 'none';
       }
     }, 3100);
   } else {
